2718bf7d4844191e91f4b9d6bdb2cf6e3f84db32
prince <princerawat7700@gmail.com>
2026-07-28 19:04:47 +0530

    update url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://chat-with-pdf-811n.onrender.com",
+  baseURL: "https://chat-with-pdf-1-tbrj.onrender.com",
 });
 
 export default api;
