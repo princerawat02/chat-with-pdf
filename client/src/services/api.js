f7226147dc137334d1db8d8fd1769207e78ebef3
prince <princerawat7700@gmail.com>
2026-07-29 10:58:27 +0530

    change baseURL to env variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://chat-with-pdf-1-tbrj.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
